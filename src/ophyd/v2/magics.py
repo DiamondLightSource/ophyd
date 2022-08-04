4fbf1467d9d8a6d4a4dfd2ffeff50d117f864571
Tom Cobb <tom.cobb@diamond.ac.uk>
2022-08-04 11:43:26 +0100

    Add movr magic

--- src/ophyd/v2/magics.py
+++ src/ophyd/v2/magics.py
@@ -1,3 +1,5 @@
+from typing import Any, List
+
 from bluesky import RunEngine
 from bluesky import plan_stubs as bps
 from IPython.core.magic import Magics, line_magic, magics_class
@@ -22,12 +24,20 @@ class OphydMagics(Magics):
     def RE(self) -> RunEngine:
         return self.shell.user_ns["RE"]
 
+    def eval(self, arg: str) -> Any:
+        return eval(arg, self.shell.user_ns)
+
+    def eval_args(self, line: str) -> List:
+        return [eval(arg, self.shell.user_ns) for arg in line.split()]
+
     @line_magic
     def mov(self, line: str):
-        args = [eval(arg, self.shell.user_ns) for arg in line.split()]
-        self.RE(bps.mv(*args))
+        self.RE(bps.mov(*self.eval_args(line)))
+
+    @line_magic
+    def movr(self, line: str):
+        self.RE(bps.movr(*self.eval_args(line)))
 
     @line_magic
     def rd(self, line: str):
-        obj = eval(line, self.shell.user_ns)
-        self.RE(print_rd(obj))
+        self.RE(print_rd(self.eval(line)))
